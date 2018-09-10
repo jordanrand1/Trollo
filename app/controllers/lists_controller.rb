8cf22bc0b3cc4d7b6ea5f702dea7e48f29909f0f
Daniel Trotter git config --globalgit config --global user.email flare84@gmail.comgit config --global user.email flare84@gmail.com <flare84@gmail.comgit config --global user.email flare84@gmail.comgit config --global user.name Daniel>
2018-09-10 16:28:52 -0600

    updated routes, list works

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,52 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :edit, :update, :destroy]
+  before_action :set_board
+
   def index
+    @lists = @board.lists.order(created_at: :asc)
   end
 
   def show
   end
 
   def new
+    @list = @board.lists.new
+  end
+
+  def create
+    @list = @board.lists.new(list_params)
+
+    if @list.save
+      redirect_to list_path
+    else
+      render :new
+    end
   end
 
   def edit
+    render :list_form
   end
+
+  def update
+    if @list.update(list_params)
+      redirect_to list_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to board_lists_path
+  end
+
+  private
+  def set_board
+    @board = Board.find(params[:board_id])
+  end
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root 'boards#index'
-  resources :boards, :tasks, :lists 
+  resources :boards do
+    resources :lists
+  end
+
+  scope 'lists/:list_id', as: 'list' do
+   resources :tasks
+ end
 end
